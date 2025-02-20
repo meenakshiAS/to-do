bcfaac218afd5711bb9ac5d60262ca01fed6fee1
Anna Makarudze <anna@makarudze.com>
2025-02-21 00:36:07 +0100

    Hide secrets

--- app/settings.py
+++ app/settings.py
@@ -10,23 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e8%o&q9*^%tbvadpu=)e59c)#5c&oy1idla(ccu5#y65jtvl0v'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG")) if os.environ.get("DEBUG") else True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "chores" if DEBUG else os.environ.get("SECRET_KEY")
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
