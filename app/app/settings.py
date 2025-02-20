426f89782ac0720f7250fe284db2533e828136dc
Anna Makarudze <anna@makarudze.com>
2025-02-21 00:08:22 +0100

    Add files for configuration and django app

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+-60rf^&(%huknfd0qtc(^b3+_i*ldw_+r8b=wp9sm8conhp5e'
+SECRET_KEY = 'django-insecure-=c0$+j+yw7xyvp@j)#mcpe&q+9t0w9olgm@u4h^hb___%%7efo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
