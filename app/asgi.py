82b75bad78fa40b1d2a06e2b6f1532824ee32759
Jammithri <kjam0045@mail.com>
2025-02-21 11:55:08 +0100

    Fix asgi.py issues

--- app/asgi.py
+++ app/asgi.py
@@ -4,11 +4,7 @@ ASGI config for app project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-=======
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
->>>>>>> 8108cd28336f9e8fdb94d562c07952987539b0dd
 """
 
 import os
