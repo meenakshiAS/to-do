aa599b11772a1fb5b4ccc282a5562719a57bc9ff
meenakshiAS <49203392+meenakshiAS@users.noreply.github.com>
2025-02-27 15:19:49 +0100

    Add Task model

--- to-do/tasks/models.py
+++ to-do/tasks/models.py
@@ -1,2 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    class Meta:
+        managed = True
+
+    def __str__(self) -> str:
+        return self.title
--- to-do/tests/conftest.py
+++ to-do/tests/conftest.py
@@ -1,8 +1,37 @@
 import pytest
+
+from django.contrib.auth.models import User
 from django.test import Client
+
+from tasks.models import Task
 
 
 @pytest.fixture
 def client(db):
     client = Client()
     return client
+
+
+@pytest.fixture
+def admin(db):
+    """Fixture to create an admin user."""
+    user = User.objects.create_superuser(
+        username="carl", 
+        email="carl@test.com", 
+        first_name="Carl", 
+        last_name="Doe", 
+        password="pass1234"
+    )
+    return user
+
+@pytest.fixture
+def task(db, admin):
+    task = Task.objects.create(
+        user=admin,
+        title="Brainstorming",
+        description="A to do app.",
+        due_date="2025-03-01",
+        completed=False
+        
+    )
+    return task
