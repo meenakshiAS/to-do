30351f7e206464343901048311dd6597a9ba16b6
payel <payelmahapatra8693@gmail.com>
2025-03-09 22:38:45 +0100

    integration test file changes

--- to-do/tests/integration_tests/test_register_user.py
+++ to-do/tests/integration_tests/test_register_user.py
@@ -12,18 +12,19 @@ def test_register_user(chrome_browser, user):
     username_input = chrome_browser.find_element(By.ID, "username")
     username_input.send_keys("test")
     time.sleep(1)
-    first_name = chrome_browser.find_element(By.ID, "first_name")
-    first_name.send_keys("Test")
-    time.sleep(1)
-    last_name = chrome_browser.find_element(By.ID, "last_name")
-    last_name.send_keys("Integration")
-    time.sleep(1)
     email = chrome_browser.find_element(By.ID, "email")
     length = 8
     random_string = str(uuid.uuid4()).replace("-", "")[:length]
     email_id = random_string + "@gmail.com"
     email.send_keys(email_id)
     time.sleep(1)
+    first_name = chrome_browser.find_element(By.ID, "first_name")
+    first_name.send_keys("Test")
+    time.sleep(1)
+    last_name = chrome_browser.find_element(By.ID, "last_name")
+    last_name.send_keys("Integration")
+    time.sleep(1)
+
     password1 = chrome_browser.find_element(By.ID, "password1")
     password1.send_keys("Test@12345678")
     time.sleep(1)
