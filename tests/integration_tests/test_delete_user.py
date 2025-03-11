0f21bb8065d5d96c0616c81a11bf6daaf73b4448
payel <payelmahapatra8693@gmail.com>
2025-03-11 19:35:44 +0100

    integration tag name change

--- tests/integration_tests/test_delete_user.py
+++ tests/integration_tests/test_delete_user.py
@@ -21,7 +21,7 @@ def test_delete_task(chrome_browser, wait, user, task_2):
     password_input.send_keys("pass1234")
     time.sleep(3)
 
-    login_button = wait.until(ec.visibility_of_element_located((By.ID, "login")))
+    login_button = wait.until(ec.visibility_of_element_located((By.NAME, "login")))
     login_button.click()
     time.sleep(3)
 
@@ -45,7 +45,7 @@ def test_delete_task(chrome_browser, wait, user, task_2):
     time.sleep(3)
 
     # Logout user
-    logout_button = wait.until(ec.visibility_of_element_located((By.ID, "logout")))
+    logout_button = wait.until(ec.visibility_of_element_located((By.NAME, "logout")))
     logout_button.click()
     time.sleep(3)
     assert "Login" in chrome_browser.title
